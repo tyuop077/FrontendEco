75f2d1b2d131025fed761ef93ef54da0bd931e23
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-02-25 01:29:48 +0300

    Implemented footer & animations

--- src/app/containers/root/Container.tsx
+++ src/app/containers/root/Container.tsx
@@ -1,6 +1,7 @@
 import styles from "./Container.module.scss";
 import {Outlet} from "react-router-dom";
 import RootHeader from "./RootHeader";
+import RootFooter from "./RootFooter";
 
 export default () => {
     return <>
@@ -8,5 +9,6 @@ export default () => {
         <div className={styles.container}>
             <Outlet />
         </div>
+        <RootFooter />
     </>
 }
---
+++ src/app/containers/root/RootFooter.tsx
@@ -0,0 +1,11 @@
+import styles from "./RootFooter.module.scss";
+
+export default () => (
+    <footer className={styles.footer}>
+        <div className={styles.container}>
+            <div className={styles.links}>
+                <span>info@ecorus.ru</span>
+            </div>
+        </div>
+    </footer>
+)
