493255267d81f659000f48ad17014fe7bf216832
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-02-25 02:12:29 +0300

    Added swiper & implemented slider

--- src/app/components/slider/Slider.tsx
+++ src/app/components/slider/Slider.tsx
@@ -1,13 +1,49 @@
 import styles from "./Slider.module.scss";
-import Recycling from "../../assets/recyclingFlat.png";
+import RecyclingFlat from "../../assets/recyclingFlat.png";
+import Trash from "../../assets/trash.png";
+import Recycling from "../../assets/recycling.png";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import {useState} from "react";
 
-export default () => <>
-    <div className={styles.slider}>
-        <div className="content">
-            <img src={Recycling} alt="Recycling" />
-            <h2>Сделаем мир чище</h2>
-            <span>Сдай макулатуру или старую одежду и получи скидку<br/>на покупку товаров из переработанных материалов</span>
-            <button>Условия сервиса</button>
+export default () => {
+    const [bgClass, setBgClass] = useState(styles.slider);
+    return (
+        <div className={bgClass}>
+            <Swiper
+                spaceBetween={50}
+                onSlideChange={(swiper) => {
+                    setBgClass(swiper.activeIndex === 1 ? styles.sliderYellow : styles.slider)
+                }}
+            >
+                <SwiperSlide>
+                    <div className="content">
+                        <img src={RecyclingFlat} alt="Recycling abstract" />
+                        <h2>Сделаем мир чище</h2>
+                        <span>Сдай макулатуру или старую одежду и получи скидку<br/>
+                            на покупку товаров из переработанных материалов</span>
+                        <button>Условия сервиса</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="content">
+                        <img src={Trash} alt="Trash" />
+                        <h2>А вы знали...</h2>
+                        <span>что среднее время разложения пластмассовых изделий колеблется
+                            от 400 до 700 лет, а полиэтиленовых пакетов — от 100 до 200 лет? </span>
+                        <button>Узнать больше</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="content">
+                        <img className="stickRight" src={Recycling} alt="Recycling showcase" />
+                        <h2>Что с масками?</h2>
+                        <span>Медицинские маски не обязательно должны становиться отходами.
+                            Их тоже можно сдать на переработку.</span><br/>
+                        <button>Пункты сбора масок</button>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
-    </div>
-</>
+    )
+}
