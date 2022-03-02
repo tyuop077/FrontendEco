b6252d4866e5868ec1ba6f14af0889817a33b175
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-03-02 15:46:39 +0300

    Finished footer

--- src/app/containers/root/RootFooter.tsx
+++ src/app/containers/root/RootFooter.tsx
@@ -1,10 +1,19 @@
 import styles from "./RootFooter.module.scss";
+import {ReactComponent as Email} from "../../assets/Email.svg";
+import {ReactComponent as Phone} from "../../assets/Phone.svg";
 
 export default () => (
     <footer className={styles.footer}>
         <div className={styles.container}>
-            <div className={styles.links}>
-                <span>info@ecorus.ru</span>
+            <div className={styles.contacts}>
+                <div>
+                    <Email />
+                    <span>info@ecorus.ru</span>
+                </div>
+                <div>
+                    <Phone />
+                    <span>+7 (800) 880-88-88</span>
+                </div>
             </div>
         </div>
     </footer>
