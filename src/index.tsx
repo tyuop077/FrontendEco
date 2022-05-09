fd491fe9f3478469d54c888001fe1696f3d88fed
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-05-09 03:00:14 +0300

    Migrated index.tsx to support React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import {StrictMode} from 'react';
-import {render} from 'react-dom';
+import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
-import { Store } from './app/Store'
-import { Provider } from 'react-redux'
+import {Store} from './app/Store'
+import {Provider} from 'react-redux'
 import './index.scss';
 import App from './App';
 
-render(
+const root = createRoot(document.getElementById('root')!);
+root.render(
   <StrictMode>
       <Provider store={Store}>
           <BrowserRouter>
               <App />
           </BrowserRouter>
       </Provider>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
