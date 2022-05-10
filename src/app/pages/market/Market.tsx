0a768c347d9fd87fe47b34df55ffc65e1626a9b9
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-05-11 02:16:03 +0300

    Implemented market items & filter

--- src/app/pages/market/Market.tsx
+++ src/app/pages/market/Market.tsx
@@ -1,13 +1,15 @@
 import styles from "./Market.module.scss";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import MarketFilter from "../../components/MarketFilter/MarketFilter";
 import {ReactComponent as EcoCoins} from "../../assets/EcoCoins.svg";
-import {CachedUser} from "../../types";
+import {APIMarketItem, CachedUser, MarketItem} from "../../types";
 import {useSelector} from "react-redux";
 import {RootState} from "../../Store";
 import {useDispatch} from "react-redux";
 import {openModal} from "../../stores/ModalSlice";
 import SignIn from "../../components/Modals/SignIn";
+import {API} from "../../services/API";
+import {Utils} from "../../Utils";
 
 export default () => {
     const [filter, setFilter] = useState({
@@ -17,7 +19,29 @@ export default () => {
         shop_ids: [] as string[]
     });
     const cachedUser: CachedUser | undefined = useSelector((state: RootState) => state.cachedUser.value);
+    const [data, setData] = useState<APIMarketItem[]>([]);
+    const [market, setMarket] = useState<MarketItem[]>([]);
     const dispatch = useDispatch();
+    useEffect(() => {
+        API.fetchMarket().then(m => setData(m))
+    }, [])
+    useEffect(() => {
+        setMarket([...data].filter(i => (
+            (filter.sexes.length === 0 || filter.sexes.includes(i.sexes[0]!)) &&
+            (filter.item_categories.length === 0 || filter.item_categories.includes(i.type)) &&
+            (filter.shop_ids.length === 0 || filter.shop_ids.includes(i.brand))
+        )).sort((a, b) => (
+            filter.sort === 0 ? b.popularity - a.popularity :
+                filter.sort === 1 ? a.price - b.price :
+                    b.createdAt - a.createdAt
+        )).map(i => ({
+            name: i.name,
+            brand: i.brand,
+            label: Utils.generateMarketLabel(i),
+            price: i.price,
+            image: i.image
+        })))
+    }, [data, filter])
     return <>
         <div className={styles.titleBar}>
             <h1>ЭкоМаркет</h1>
@@ -27,10 +51,12 @@ export default () => {
                         <button
                             className={i === filter.sort ? "Active" : undefined}
                             onClick={() => setFilter({...filter, sort: i})}
+                            key={i}
                         >
                             {value}
                         </button>
-                    )}
+                    )
+                }
             </div>
         </div>
         <div className={styles.market}>
@@ -52,7 +78,7 @@ export default () => {
                     />
                     <MarketFilter
                         label="Брэнд"
-                        options={{"0": "H&M", "1": "P&B", "2": "Adidas", "3": "Nike", "4": "Reebok"}}
+                        options={{"H&M": "H&M", "P&B": "P&B", "Adidas": "Adidas", "NIKE": "Nike", "Reebok": "Reebok"}}
                         onChange={v => setFilter({...filter, shop_ids: v})}
                         selected={filter.shop_ids}
                     />
@@ -80,6 +106,20 @@ export default () => {
                         <button onClick={() => dispatch(openModal(<SignIn />))}>Войти</button>
                     </div>
                 )}
+                {market.map(i => (
+                    <div className="item" key={i.name}>
+                        <div className="cover">
+                            <span>{i.brand}</span>
+                            <img src={i.image} alt={i.name} />
+                        </div>
+                        <b>{i.name}</b>
+                        <span>{i.label}</span>
+                        <div className="price">
+                            <EcoCoins />
+                            {i.price}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     </>
